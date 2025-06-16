e0caaabbcd08a06fb56758642a9994e6f97170d0
Adyllson <domingosady11son@gmail.com>
2025-06-16 17:47:08 +0100

    finalizando o repositorio de blog

---
+++ src/backend/Kairos.Infrastructure/Repositories/BlogRepository.cs
@@ -0,0 +1,264 @@
+namespace Kairos.Infrastructure.Repositories;
+public class BlogRepository(AppDbContext context) : IBlogRepository
+{
+    #region </Create>
+        public async Task<Result<BlogEntity>> CreateAsync(BlogEntity entity, CancellationToken token)
+        {
+            try
+            {
+                if(entity == null)
+                {
+                    return new Result<BlogEntity>(
+                        null, 
+                        400, 
+                        "Parâmetros não podem estar vazio."
+                        );
+                }
+                await context.Blogs.AddAsync(entity, token);
+                return new Result<BlogEntity>(
+                    entity, 
+                    201, 
+                    "Operação executada com sucesso."
+                    );
+            }
+            catch (Exception ex)
+            {
+                return new Result<BlogEntity>(
+                    null, 
+                    500, 
+                    $"Erro ao executar a operação (CRIAR). Erro {ex.Message}."
+                    );
+            }
+        }
+    #endregion
+
+    #region </Delete>
+        public async Task<Result<bool>> DeleteAsync(int entityId, CancellationToken token)
+        {
+            try
+            {
+                if (entityId <= 0)
+                {
+                    return new Result<bool>(
+                        false, 
+                        400, 
+                        "ID deve ser maior que zero."
+                        );
+                }
+                var response = await context.Blogs.FirstOrDefaultAsync(x => x.Id == entityId, token);
+                if (response == null)
+                {
+                    return new Result<bool>(
+                        false, 
+                        404, 
+                        "ID não encontrado."
+                        );
+                }
+                context.Blogs.Remove(response);
+                return new Result<bool>(
+                    true, 
+                    200, 
+                    "Operação executada com sucesso."
+                    );
+            }
+            catch (Exception ex)
+            {
+                return new Result<bool>(
+                    false, 
+                    500, 
+                    $"Erro ao executar a operação (DELETAR). Erro {ex.Message}."
+                    );
+            }
+        }
+    #endregion
+
+    #region </GetAll>
+        public async Task<PagedList<List<BlogEntity>?>> GetAllAsync(PagedRequest request, CancellationToken token)
+        {
+            try
+            {
+                var query = context.Blogs.AsNoTracking().Include(x => x.Usuario).AsQueryable();
+
+                var result = await query
+                            .Skip((request.PageNumber - 1) * request.PageSize)
+                            .Take(request.PageSize)
+                            .ToListAsync();
+                
+                var count = await query.CountAsync();
+
+                return new PagedList<List<BlogEntity>?>(
+                    result,
+                    count,
+                    request.PageNumber,
+                    request.PageSize
+                );
+            }
+            catch (Exception ex)
+            {
+                return new PagedList<List<BlogEntity>?>(
+                    null, 
+                    500, 
+                    $"Erro ao executar a operação (GET ALL). Erro {ex.Message}."
+                    );
+            }
+        }
+    #endregion
+
+    #region </GetById>
+        public async Task<Result<BlogEntity?>> GetByIdAsync(int entityId, CancellationToken token)
+        {
+            try
+            {
+                if(entityId <= 0)
+                {
+                    return new Result<BlogEntity?>(
+                        null, 
+                        400, 
+                        "ID deve ser maior que zero."
+                        );
+                }
+                var response = await context.Blogs.Include(x => x.Usuario).FirstOrDefaultAsync(x => x.Id == entityId, token);
+                if(response == null)
+                {
+                    return new Result<BlogEntity?>(
+                        null, 
+                        404, 
+                        "ID não encontrado."
+                        );
+                }
+                return new Result<BlogEntity?>(
+                    response, 
+                    200, 
+                    "Dados encontrado."
+                    );
+            }
+            catch (Exception ex)
+            {
+                return new Result<BlogEntity?>(
+                    null, 
+                    500, 
+                    $"Erro ao executar a operação (GET BY ID). Erro {ex.Message}."
+                    );
+            }
+        }
+    #endregion
+
+    #region </GetFile>
+        public async Task<Result<BlogEntity?>> GetFileAsync(int entityId, CancellationToken token)
+        {
+            try
+            {
+                if(entityId <= 0)
+                {
+                    return new Result<BlogEntity?>(
+                        null, 
+                        400, 
+                        "ID deve ser maior que zero."
+                        );
+                }
+                var response = await context.Blogs.Include(x => x.Usuario).FirstOrDefaultAsync(x => x.Id == entityId, token);
+                if(response == null)
+                {
+                    return new Result<BlogEntity?>(
+                        null, 
+                        404, 
+                        "ID não encontrado."
+                        );
+                }
+                return new Result<BlogEntity?>(
+                    response, 
+                    200, 
+                    "Dados encontrado."
+                    );
+            }
+            catch (Exception ex)
+            {
+                return new Result<BlogEntity?>(
+                    null, 
+                    500, 
+                    $"Erro ao executar a operação (GET BY ID). Erro {ex.Message}."
+                    );
+            }
+        }
+    #endregion
+
+    #region </Search>
+        public async Task<Result<List<BlogEntity>?>> SearchAsync(Expression<Func<BlogEntity, bool>> expression, string entity, CancellationToken token)
+        {
+            try
+            {
+                if(entity == null)
+                {
+                    return new Result<List<BlogEntity>?>(
+                        null, 
+                        400, 
+                        "Parâmetros não podem estar vazio."
+                        );
+                }
+                var response = await context.Blogs.Include(x => x.Usuario).Where(expression).ToListAsync(token);
+                if(response == null || response.Count == 0)
+                {
+                    return new Result<List<BlogEntity>?>(
+                        null, 
+                        404, 
+                        "Nenhum dado encontrado."
+                        );
+                }
+
+                return new Result<List<BlogEntity>?>(
+                    response, 
+                    200, 
+                    "Dados encontrado."
+                    );
+            }
+            catch (Exception ex)
+            {
+                return new Result<List<BlogEntity>?>(
+                    null, 
+                    500, 
+                    $"Erro ao executar a operação (SEARCH). Erro {ex.Message}."
+                    );
+            }
+        }
+    #endregion
+
+    #region </Update>
+        public async Task<Result<BlogEntity>> UpdateAsync(BlogEntity entity, CancellationToken token)
+        {
+            try
+            {
+                if(entity == null)
+                {
+                    return new Result<BlogEntity>(
+                        null, 
+                        400, 
+                        "Parâmetros não podem estar vazio."
+                        );
+                }
+                var response = await context.Blogs.FindAsync(entity.Id);
+                if(response == null)
+                {
+                    return new Result<BlogEntity>(
+                        null, 
+                        404, 
+                        "ID não encontrado."
+                        );
+                }
+                context.Entry(response).CurrentValues.SetValues(entity);
+                return new Result<BlogEntity>(
+                    response, 
+                    200, 
+                    "Operação executada com sucesso."
+                    );
+            }
+            catch (Exception ex)
+            {
+                return new Result<BlogEntity>(
+                    null, 
+                    500, 
+                    $"Erro ao executar a operação (UPDATE). Erro {ex.Message}."
+                    );
+            }
+        }
+    #endregion
+}
